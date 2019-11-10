1dfacb7d6cbc1b2b63a8c87742880fdbb5491589
UtpalMaiti <40532995+UtpalMaiti@users.noreply.github.com>
2019-11-10 17:43:33 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,11 @@
-[200~class Simple{  
+public class Simple{  
 
 //bdsjfjhfbhjbfhbhjdfbjgdbvf
     public static void main(String args[]){  
      System.out.println("Hello Java");  
+        
+          System.out.println("Hello Utpal");  
+          System.out.println("Welcome to Git");  
     }  
 }  
 
